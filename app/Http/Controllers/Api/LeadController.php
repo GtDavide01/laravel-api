f08a20e70234f506cf16d8babd1e7d4b9363632b
GtDavide01 <davidefiorini01@gmail.com>
2023-01-24 14:49:20 +0100

    Add  : response false or true in LeadController

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,6 +7,7 @@ use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
@@ -14,10 +15,29 @@ class LeadController extends Controller
     {
         $data = $request->all();
 
+        // validazione dei dati inseriti
+        $validator = Validator::make($data, [
+            'name' => ['required', 'max:150'],
+            'email' => ['required', 'email', 'max:200'],
+            'messaggio' => ['required']
+        ]);
+
+        // se i dati non sono validi restitusco gli errori con success false 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+
         $new_contact = new Lead();
         $new_contact->fill($data);
         $new_contact->save();
 
         Mail::to('info@boolfolio.com')->send(new NewContact($new_contact));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
